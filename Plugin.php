7540daae8d5b44db52a3364dd230c0085d22a385
Damian Lewis <damian@damianlewis.co.uk>
2019-11-04 18:00:48 +0000

    Updated readme

--- Plugin.php
+++ Plugin.php
@@ -4,36 +4,21 @@ namespace DamianLewis\CookieConsent;
 
 use System\Classes\PluginBase;
 
-/**
- * CookieConsent Plugin
- *
- * @package DamianLewis\CookieConsent
- */
 class Plugin extends PluginBase
 {
     const COOKIE_NAME = 'cookieconsent_status';
     const COOKIE_STATUS_ALLOW = 'allow';
 
-    /**
-     * Returns information about this plugin.
-     *
-     * @return array
-     */
     public function pluginDetails(): array
     {
         return [
-            'name'        => 'Cookie Consent',
+            'name' => 'Cookie Consent',
             'description' => 'Checks the status of the consent cookie.',
-            'author'      => 'Damian Lewis',
-            'icon'        => 'icon-legal'
+            'author' => 'Damian Lewis',
+            'icon' => 'icon-legal'
         ];
     }
 
-    /**
-     * Registers CMS markup tags introduced by this plugin.
-     *
-     * @return array
-     */
     public function registerMarkupTags(): array
     {
         return [
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# oc-cookieconsent-plugin
+# Cookie Consent plugin for October
+Adds a frontend function for checking status of cookie consent.
